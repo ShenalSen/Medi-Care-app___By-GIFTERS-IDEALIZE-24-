052683f6fcda898365a5f2131efc0b2050ff5cfd
ShenalSen <136364310+ShenalSen@users.noreply.github.com>
2024-05-20 18:42:57 +0530

    Added to Page files
    # Medi-Care-app
    This project aims to develop a user-friendly medical website - 'Medi Care' offering accessible and trustworthy information on common health issues.

---
+++ script_1.js
@@ -0,0 +1,50 @@
+
+const { createApp } = Vue;
+
+const app = createApp({
+    data() {
+        return {
+            navLinks: [
+                { text: 'Home', href: 'home.html' },
+                { text: 'Community', href: 'community.html' },
+                { text: 'Treatment', href: 'treatment.html' },
+                { text: 'Connect', href: 'contact.html' }
+            ]
+        };
+    }
+});
+app.mount('#bar');
+
+
+const tag = Vue.createApp({
+    data() {
+        return {
+            message1: 'Your companion',
+            message2: 'In quality diagnosis'
+        };
+    }
+});
+tag.mount('#message');
+
+const prompt = Vue.createApp({
+    data() {
+        return {
+            prompte: "or take our SELF-ASSESMENT QUIZ...",
+        };
+    }
+});
+prompt.mount('#prompt');
+
+
+const login = Vue.createApp({
+    data() {
+      return {
+        signInLink: "login.html", 
+        loginLink: "login.html"
+      };
+    }
+  });
+login.mount('#login');
+  
+
+
