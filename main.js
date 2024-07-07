a6249d47d88ff2c0ecd30d684fc50c366c6114c0
SNBlazei <sanjula.20230812@iit.ac.lk>
2024-07-07 19:42:12 +0530

    added results

---
+++ main.js
@@ -0,0 +1,45 @@
+new Vue({
+    el: '#app',
+    data: {
+        results: [
+            {
+                id: 1,
+                user: 'user 12368442',
+                posted: 'posted 10 mins ago',
+                content: 'What should I do during an asthma or allergic reaction?',
+                stars: 14,
+                comments: 'No comments yet',
+                tags: '@asthma @allergies'
+            },
+            {
+                id: 2,
+                user: 'user 12368442',
+                posted: 'posted 10 mins ago',
+                content: 'How can I manage pet allergies.....??',
+                stars: 14,
+                comments: 'No comments yet',
+                tags: '@allergies'
+            },
+            {
+                id: 3,
+                user: 'user 12368442',
+                posted: 'posted 10 mins ago',
+                content: 'What triggers asthma symptoms?',
+                stars: 14,
+                comments: 'No comments yet',
+                tags: '@asthma'
+            },
+            {
+                id:4,
+                user:'user 12368442',
+                posted: 'posted 10 mins ago',
+                content: 'How often should I see my doctor asthma?',
+                stars:14,
+                comments:'No comments yet',
+                tags:'@asthma @allergies'
+
+
+            }
+        ]
+    }
+});
