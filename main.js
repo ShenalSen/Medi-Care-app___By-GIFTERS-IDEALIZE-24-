4ab59be9fca6a43eb79d4d23565be128e04fe33e
SNBlazei <sanjula.20230812@iit.ac.lk>
2024-07-09 09:17:23 +0530

    edited
    Co-Authored-By: Shenal Senaratne <136364310+ShenalSen@users.noreply.github.com>

--- main.js
+++ main.js
@@ -3,53 +3,6 @@ new Vue({
     data: {
         results: [
             {
-<<<<<<< Updated upstream
-                id: 1,
-                user: 'user 12368442',
-                posted: 'posted 10 mins ago',
-                content: 'What should I do during an asthma or allergic reaction?',
-                stars: 14,
-                comments: 'No comments yet',
-                tags: '@asthma @allergies'
-            },
-            {
-                id: 2,
-                user: 'user 12368442',
-                posted: 'posted 10 mins ago',
-                content: 'How can I manage pet allergies.....??',
-                stars: 14,
-=======
-                id: 2,
-                user: 'user 128442',
-                posted: 'posted 5 mins ago',
-                content: 'How can I manage pet allergies.....??',
-                stars: 16,
->>>>>>> Stashed changes
-                comments: 'No comments yet',
-                tags: '@allergies'
-            },
-            {
-<<<<<<< Updated upstream
-                id: 3,
-                user: 'user 12368442',
-                posted: 'posted 10 mins ago',
-                content: 'What triggers asthma symptoms?',
-                stars: 14,
-                comments: 'No comments yet',
-                tags: '@asthma'
-            },
-            {
-                id:4,
-                user:'user 12368442',
-                posted: 'posted 10 mins ago',
-                content: 'How often should I see my doctor asthma?',
-                stars:14,
-                comments:'No comments yet',
-                tags:'@asthma @allergies'
-
-
-            }
-=======
                 id: 2,
                 user: 'user 128442',
                 posted: 'posted 5 mins ago',
@@ -112,7 +65,15 @@ new Vue({
                 comments: 'No comments yet',
                 tags: '@allergies'
             },
->>>>>>> Stashed changes
+            {
+                id: 2,
+                user: 'user 128442',
+                posted: 'posted 5 mins ago',
+                content: 'How can I manage pet allergies.....??',
+                stars: 16,
+                comments: 'No comments yet',
+                tags: '@allergies'
+            },
         ]
     }
 });
